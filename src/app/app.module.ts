62c8195d1472a1c556132acb371330721345f636
Jesus <jesusperez@seocontenidos.net>
2021-09-27 11:21:55 -0400

    android bundle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SpinnerInterceptor } from './spinner/spinner.interceptor';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,TruncatePipe],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     HttpClientModule, DataTablesModule,
-  SpinnerComponentModule],
+  SpinnerComponentModule,],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
               { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true}],
   bootstrap: [AppComponent],
